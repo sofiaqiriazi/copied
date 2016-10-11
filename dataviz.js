73fa138d0e1e6c050b1ca2c86cc0de09944e28c9
s1790986 <s.kyriaziorqirjazi-1@student.utwente.nl>
2016-10-11 14:46:30 +0200

    data source folder

--- dataviz.js
+++ dataviz.js
@@ -7,7 +7,7 @@ var countByDateBarChart;
 var dataTable;
 
 // load the data file
-d3.csv("file:///Users/colm/dev/code/dataviz/data/PPR-mini.csv", function(houseSales) {
+d3.csv("data/PPR-mini.csv", function(houseSales) {
 
 	// associate the charts with their html elements
 	yearRowChart = dc.rowChart("#chart-ring-years");
